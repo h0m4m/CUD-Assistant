52bc9775b4ee929a98833caa5ffdf6d6a4de94a6
h0m4m <mhdhomam2003@gmail.com>
2023-10-05 15:48:50 +0400

    changes

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 import os
-import sys
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
-from flask import Flask, render_template, request
-import time
-
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from flask import Flask, render_template, request, jsonify
 
 os.environ["OPENAI_API_KEY"] = "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"
 app = Flask(__name__)
 
+chat_history = []
+
+# Create and set up the index and conversational retrieval chain
+loader = TextLoader('data.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+)
+
 @app.route('/')
-def index ():
+def index():
     return render_template('index.html')
-@app.route('/api', methods = ["POST"])
+
+@app.route('/api', methods=["POST"])
 def api():
     message = request.json.get("message")
-    loader = TextLoader('data.txt')
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    response = index.query(message)
-    return response
-        
-    
+    result = chain({"question": message, "chat_history": chat_history})
+    response = result["answer"]
+    chat_history.append((message, response))
+    return jsonify({"content": response})
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,49 +1,85 @@
 const side_panel = document.querySelector("#Slider_Menu");
 const checkbox = document.querySelector('input[type="checkbox"]');
 const Left_panel = document.querySelector(".Left_panel_icon_open_cont");
-const Nav_Title_Cont = document.querySelector('.Nav_Title_Cont');
+const Nav_Title_Cont = document.querySelector('.Nav_Title_Cont'); 
 const CUD_Nav_Logo_Cont = document.querySelector('.CUD_Nav_Logo_Cont');
 const blurscreen = document.querySelector('.blurscreenelement');
+const Commandbuttons = document.querySelectorAll('.Messaging_Command');
+let clickedButtonText = '';
 
-let panel_open_toggle = false; // Start with the panel closed
+Commandbuttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Store the text of the clicked button in the variable
+        let innerText = ''
+        if (button.classList.contains('M_C12') || button.classList.contains('M_C11')) {
+             innerText = document.querySelector('.M_C11 span').textContent;
+        }
+        else if (button.classList.contains('M_C22') || button.classList.contains('M_C21')) {
+             innerText = document.querySelector('.M_C21 span').textContent;
+        }
+        else if (button.classList.contains('M_C32') || button.classList.contains('M_C31')) {
+             innerText = document.querySelector('.M_C31 span').textContent;
+        }
+        else if (button.classList.contains('M_C42') || button.classList.contains('M_C41')) {
+             innerText = document.querySelector('.M_C41 span').textContent;
+        }
+        console.log(innerText);
+        document.querySelector('.Messaging_Commands_Section').style.display = 'none';
+        document.querySelector('.Messaging_Commands_Section_small').style.display = 'none';
+        document.querySelector('.Messagin')
+        document.querySelector('#Messaging_Section').style.height = '70px';
+        
+        if(window.innerWidth <= 480){
+            document.querySelector('#Chat_Section').style.height = 'calc(100% - 132px)';
+        }
+        else{
+            document.querySelector('#Chat_Section').style.height = 'calc(100% - 137px)';
+        }
 
-// Function to toggle the checkbox
+    });
+});
+
+
+let panel_open_toggle = false; 
+
 function toggleCheckbox() {
     checkbox.checked = !checkbox.checked;
 }
 
-// Add a click event listener to the button to toggle the checkbox and open/close the panel
+
 Left_panel.addEventListener("click", () => {
-    // Toggle the checkbox
+
     toggleCheckbox();
 
-    // Toggle the panel open/close
+
     if (panel_open_toggle === false) {
-        if (window.innerWidth < 427) {
+        if(window.innerWidth < 427){
             blurscreen.classList.add("blurscreen");
         }
 
 
-        if (window.innerWidth <= 660) {
+        if(window.innerWidth <= 660){
             Nav_Title_Cont.style.display = "none";
             CUD_Nav_Logo_Cont.style.display = "none";
         }
-        side_panel.style.left = "0px";
-        if (window.innerWidth < 375 && window.innerWidth > 330) {
+        side_panel.style.left = "0px";  
+        if(window.innerWidth < 375 && window.innerWidth > 330){
             Left_panel.style.marginLeft = "275px";
-        } else if (window.innerWidth < 330) {
+        }
+        else if(window.innerWidth < 330){
             Left_panel.style.marginLeft = "250px";
-        } else {
+        }
+        else{
             Left_panel.style.marginLeft = "300px";
         }
 
         panel_open_toggle = true;
     } else {
 
-        if (window.innerWidth < 427) {
+        if(window.innerWidth < 427){
             blurscreen.classList.remove("blurscreen");
         }
-        if (window.innerWidth <= 660) {
+        if(window.innerWidth <= 660){
             Nav_Title_Cont.style.display = "block";
             CUD_Nav_Logo_Cont.style.display = "block";
         }
@@ -53,74 +89,76 @@ Left_panel.addEventListener("click", () => {
     }
 });
 
-// Add a click event listener to the checkbox itself to prevent propagation
+
 checkbox.addEventListener("click", (e) => {
-    e.stopPropagation(); // Prevent the click event from reaching the parent button
+    e.stopPropagation(); 
 });
 
-// Add a click event listener to the label to toggle the checkbox as well
+
 const label = document.querySelector(".hamburger");
 label.addEventListener("click", () => {
     toggleCheckbox();
 });
 
-// Function to update the element styles based on window width
+
 function updateElementStyles() {
     if (window.innerWidth <= 660) {
-        if (side_panel.style.left == "0px") {
+        if(side_panel.style.left == "0px"){
             Nav_Title_Cont.style.display = "none";
             CUD_Nav_Logo_Cont.style.display = "none";
-        } else {
+        }
+        else{
             Nav_Title_Cont.style.display = "block";
             CUD_Nav_Logo_Cont.style.display = "block";
         }
-    } else {
+    }
+    else{
         Nav_Title_Cont.style.display = "block";
         CUD_Nav_Logo_Cont.style.display = "block";
     }
 }
-// Add an event listener to the window to call the updateElementStyles function on window resize
+
 window.addEventListener("resize", updateElementStyles);
 
-// Initial call to updateElementStyles to set the initial state
+
 updateElementStyles();
 
 
 
 const send_button_icon = document.querySelector(".send_button_icon");
 
-// Rest of your code...
 const Chat_Section = document.querySelector("#Chat_Section");
 const User_Input = document.querySelector(".User_Input");
 const send_button = document.querySelector(".send_button");
 
 
-User_Input.addEventListener("input", () => {
-    if (User_Input.value == "") {
+User_Input.addEventListener("input", ()=>{
+    if(User_Input.value == ""){
         send_button_icon.src = "static/img/mapleleaficonoutline.png";
-    } else {
+    }
+    else{
         send_button_icon.src = "static/img/mapleleaficonfill.png";
     }
 });
 
 function addMessage(message, isUserMessage) {
-    const messageDiv = document.createElement("div");
+  const messageDiv = document.createElement("div");
 
-    if (isUserMessage) {
-        messageDiv.classList.add("User_Message");
+  if (isUserMessage) {
+    messageDiv.classList.add("User_Message");
 
-    } else {
-        messageDiv.classList.add("AI_Message");
-    }
+  } else {
+    messageDiv.classList.add("AI_Message");
+  }
 
-    messageDiv.innerHTML = message;
+  messageDiv.innerHTML = message;
 
     Chat_Section.appendChild(messageDiv);
     Chat_Section.scrollTo(0, Chat_Section.scrollHeight);
 }
 
-function sendMessage() {
-    const message = User_Input.value.trim();
+function sendMessage(message) {
+
 
     if (message !== "") {
         addMessage(message, true);
@@ -136,21 +174,22 @@ function sendMessage() {
         })
         .then(response => response.json())
         .then(data => {
-
             const content = data.content;
             addMessage(content, false);
             console.log(content);
-        })
+        }).catch(error => console.error(error));
 }
 
 
 
 
+    
 send_button.addEventListener("click", sendMessage);
 User_Input.addEventListener("keydown", event => {
 
-    if (event.keyCode === 13 && !event.shiftKey) {
+    if(event.keyCode === 13 && !event.shiftKey){
         event.preventDefault();
-        sendMessage();
+        
+        sendMessage(User_Input.value.trim());
     }
 });
